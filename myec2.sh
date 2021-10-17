41f7f3fb988b0021b4da1463244b2c27736c681a
gugas1250 <88409939+gugas1250@users.noreply.github.com>
2021-10-17 07:32:29 -0700

    Update myec2.sh

--- myec2.sh
+++ myec2.sh
@@ -1,1 +1,2 @@
- aws ec2 run-instances --image-id ami-013a129d325529d4d  --count 1 --instance-type t2.micro --key-name new-gugas --region us-west-2
+#!/bin/bash
+aws ec2 run-instances --image-id ami-013a129d325529d4d  --count 1 --instance-type t2.micro --key-name new-gugas --region us-west-2
