3699bc94f8bdfceaf6e998a9d7e58145a5acc3f0
gugas1250 <88409939+gugas1250@users.noreply.github.com>
2021-10-17 07:58:12 -0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -21,7 +21,7 @@ profile = "default"
 
 resource "aws_instance" "my-first-ec2" {
 ami = "ami-0b0af3577fe5e3532"
-instance_type = "t3.micro"
+instance_type = "t2.micro"
 tags = {
 Name = "myec2-1"
 Owner = "paulo"
